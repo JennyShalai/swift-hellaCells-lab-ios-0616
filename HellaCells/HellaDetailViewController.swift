e10e75436916ad77ccaea5ed43b1fbce87668c13
JennyShalai <jenny.shalai@gmail.com>
2016-12-18 20:20:08 -0500

    load 100 cells: add TVC and VC, segue, func to init 100 cells

--- HellaCells/HellaDetailViewController.swift
+++ HellaCells/HellaDetailViewController.swift
@@ -1,25 +1,25 @@
 //
-//  ViewController.swift
+//  HellaDetailViewController.swift
 //  HellaCells
 //
-//  Created by Flatiron School on 8/8/16.
+//  Created by Flatiron School on 12/18/16.
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class HellaDetailViewController: UIViewController {
+    
+    @IBOutlet weak var infoLabel: UILabel!
 
+    var number: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.infoLabel.text = String(self.number)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
